12d213b604a8c7218c51a5e7002bba285ccd4093
chouchouji <70570907+chouchouji@users.noreply.github.com>
2024-09-02 06:07:26 +0000

    refactor: optimize vite config code

--- vite.config.ts
+++ vite.config.ts
@@ -4,38 +4,46 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import externalGlobals from "rollup-plugin-external-globals";
 
-const isProduction = process.env.NODE_ENV === 'production'
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [
-    vue(),
-    isProduction && externalGlobals({
+export default defineConfig(({ command }) => {
+  const isBuildCommand = command === 'build'
+
+  const plugins = [
+    vue()
+  ]
+
+  if (isBuildCommand) {
+    plugins.push(externalGlobals({
       vue: "Vue",
       echarts: 'echarts',
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  build: {
-    minify: false,
-    rollupOptions: {
-      external: ['vue', 'echarts'],
-      output: {
-        dir: 'docs',
-        manualChunks(id) {
-          if (id.includes('src/utils')) {
-            return 'utils'
-          }
+    }))
+  }
 
-          if (id.includes('node_modules/lodash-es')) {
-            return 'lodash'
+  return {
+    base: './',
+    plugins,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    build: {
+      minify: false,
+      rollupOptions: {
+        external: ['vue', 'echarts'],
+        output: {
+          dir: 'docs',
+          manualChunks(id) {
+            if (id.includes('src/utils/utils-')) {
+              return 'utils'
+            }
+
+            if (id.includes('node_modules/lodash-es')) {
+              return 'lodash'
+            }
           }
         }
       }
-    }
-  },
+    },
+  }
 })
